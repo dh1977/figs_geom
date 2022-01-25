d3521cd442b2dedcb64acf135338376c08b242a9
Administrador <Administrador@dh>
2022-01-25 05:01:47 -0400

    figuras geométricas

---
+++ src/Cuadrado.java
@@ -0,0 +1,25 @@
+public class Cuadrado extends FiguraGeometrica{
+    
+    private double lado;
+    
+    public Cuadrado(double l){
+        super("cuadrado");
+        this.lado = l;
+    }
+    
+    @Override
+    public double getArea() {
+        return this.lado * this.lado;
+    }
+    
+    @Override
+     public double getPerimetro() {
+        return 4 * this.lado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Cuadrado{" + "lado=" + lado + '}';
+    }
+        
+}
---
+++ src/FiguraGeometrica.java
@@ -0,0 +1,39 @@
+public class FiguraGeometrica {
+    private String nombreFigura;
+    private double area ;
+    private double perimetro;
+    
+    public FiguraGeometrica(String nombre){
+        this.nombreFigura = nombre;
+    }
+
+    public String getNombreFigura() {
+        return nombreFigura;
+    }
+
+    public void setNombreFigura(String nombreFigura) {
+        this.nombreFigura = nombreFigura;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    //@Override
+    public String toString() {
+        return "FiguraGeometrica{" + "nombreFigura=" + nombreFigura + ", area=" + area + ", perimetro=" + perimetro + '}';
+    }
+}
+
