d5b35669553340b7cf6f7de2f63b1d66b3b48c42
Administrador <Administrador@dh>
2022-01-31 17:39:53 -0400

    sort method

--- src/Ppal.java
+++ src/Ppal.java
@@ -15,49 +15,71 @@ class Shop {
         this.name = name;
         this.stock = stock;
     }
+    
+    int getStock() {
+    	return this.stock;
+    }
+    
+    String getName() {
+    	return this.name;
+    }
+    
+    void setName(String n) {
+    	this.name = n;
+    }
 }
   
 // creates the comparator for comparing stock value
-class StockComparator implements Comparator<Shop> {
-  
-    // override the compare() method
-    public int compare(Shop s1, Shop s2)
-    {
-        if (s1.stock == s2.stock)
-            return 0;
-        else if (s1.stock > s2.stock)
-            return 1;
-        else
-            return -1;
-    }
-}
+//class StockComparator implements Comparator<Shop> {
+//  
+//    // override the compare() method
+//    public int compare(Shop s1, Shop s2)
+//    {
+//        if (s1.stock == s2.stock)
+//            return 0;
+//        else if (s1.stock > s2.stock)
+//            return 1;
+//        else
+//            return -1;
+//    }
+//}
   
 class Ppal {
     public static void main(String[] args)
     {
         // create the ArrayList object
         ArrayList<Shop> s = new ArrayList<Shop>();
+        s.add(new Shop(423, "Books", 101));
         s.add(new Shop(218, "Pen", 520));
         s.add(new Shop(223, "Pencil", 213));
-        s.add(new Shop(423, "Books", 101));
         s.add(new Shop(512, "Toy", 59));
         s.add(new Shop(723, "Bottle", 10));
   
+        @SuppressWarnings("unchecked")
+		ArrayList<Shop> s2 = (ArrayList<Shop>) s.clone();
+        
         System.out.println("before sorting");
         for (Shop shop : s) {
             System.out.println(shop.stock + " " + shop.name
                                + " " + shop.ProductNo);
         }
-        System.out.println();
-  
-        System.out.println(
-            "After sorting(sorted by Stock)");
+        
+        System.out.println("\nAfter sorting(sorted by Stock)");
   
         // call the sort function
-        Collections.sort(s, new StockComparator());
+//        Collections.sort(s, new StockComparator());
+//        Collections.sort(s,Comparator.comparing(Shop::getStock));
+       	s.sort(Comparator.comparing(Shop::getStock)); 
         for (Shop shop : s) {
             System.out.println(shop.stock + " " + shop.name
                                + " " + shop.ProductNo);
+            shop.setName(shop.name+"*");
         }
+        System.out.println("---");
+        for (Shop shop : s2) {
+            System.out.println(shop.stock + " " + shop.name +  " " + shop.ProductNo);
+        }
+
+       
     }
 }
